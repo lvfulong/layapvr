511a893c615b803f07e0d2af2e42a48ab1840b9f
lvfulong <775331175@qq.com>
2018-02-05 15:36:25 +0800

    commit

--- layaPvrCommand.ts
+++ layaPvrCommand.ts
@@ -30,12 +30,45 @@ export var builder = {
     choices: ['PVRTCI_2BPP_RGB', 'PVRTCI_4BPP_RGB', 'PVRTCI_2BPP_RGBA', 'PVRTCI_4BPP_RGBA', 'ETC1_RGB', 'ETC2_RGB', 'ETC2_RGBA'],
     required: true,
     requiresArg: true,
-    description: '像素格式，必选'
-  }
+    description: '像素格式,同TexturePacker--opt,必选,可选'
+  },
+  extrude:
+  {
+    required: false,
+    requiresArg: true,
+    description: '同TexturePacker --extrude'
+  },
+  etc1quality:
+  {
+    choices: ['low', 'low-perceptual', 'high', 'high-perceptual'],
+    required: false,
+    requiresArg: true,
+    description: '同TexturePacker --etc1-quality,可选'
+  },
+  etc2quality:
+  {
+    choices: ['low', 'low-perceptual', 'high', 'high-perceptual'],
+    required: false,
+    requiresArg: true,
+    description: '同TexturePacker --etc2-quality,可选'
+  },
+  pvrquality:
+  {
+    choices: ['verylow', 'low', 'normal', 'high','best'],
+    required: false,
+    requiresArg: true,
+    description: '同TexturePacker --pvr-quality,可选'
+  },
+  dithertype:
+  {
+    choices: ['NearestNeighbour', 'Linear', 'FloydSteinberg', 'FloydSteinbergAlpha','Atkinson','AtkinsonAlpha'],
+    required: false,
+    requiresArg: true,
+    description: '同TexturePacker --dither-type,可选'
+  },
 }
 
 exports.handler = async function (argv) {
-
   let inputDir = getAbsPath(argv.inputdir);
   if (!fs.existsSync(inputDir)) {
     console.log('错误! 找不到输入目录 ' + inputDir);
@@ -59,72 +92,56 @@ exports.handler = async function (argv) {
     let outputTexturePath = path.join(outputDir, path.parse(file).name + '.pvr');
 
     let cmd = "TexturePacker ";
-
     cmd += "\"" + inputTexturePath + "\"";
-
     cmd += " --sheet ";
     cmd += "\"" + outputTexturePath + "\"";
-
     cmd += " --texture-format pvr3 ";
-
-    cmd += " --format xml --extrude 5";//TODO
+    cmd += " --format xml";
     cmd += " --data ";
     let outputDataPath = path.join(outputDir, path.parse(file).name + '.xml');
     cmd += "\"" + outputDataPath + "\"";
-
     cmd += " --size-constraints POT ";
     cmd += " --alpha-handling PremultiplyAlpha ";
     cmd += " --opt ";
     cmd += "\"" + argv.format + "\"";
-
+    //格式为PVR，不是ETC，强制为方的
     if (isPVRTC(argv.format)) {
       cmd += " --force-squared ";
     }
-
-    child_process.execSync(cmd);
-
+    if (argv.extrude) {
+      cmd += " --extrude ";
+      cmd += "\"" + argv.extrude + "\"";
+    }
+    if (argv.etc1quality) {
+      cmd += " --etc1-quality ";
+      cmd += "\"" + argv.etc1quality + "\"";
+    }
+    if (argv.etc2quality) {
+      cmd += " --etc2-quality ";
+      cmd += "\"" + argv.etc2quality + "\"";
+    }
+    if (argv.pvrquality) {
+      cmd += " --pvr-quality ";
+      cmd += "\"" + argv.pvrquality + "\"";
+    }
+    if (argv.dithertype) {
+      cmd += " --dither-type ";
+      cmd += "\"" + argv.dithertype + "\"";
+    }
+    child_process.execSync(cmd); 
+    
+    cmd = "..\\tools\\win\\PVRTool.exe ";
+    cmd += "\"" + outputTexturePath + "\" ";
+    cmd += "\"" + path.join(outputDir, path.parse(file).name) + "\" ";
     let x = 5;
     let y = 5;
     let width = 20;
     let height = 20;
-    let originBuffer = fs.readFileSync(outputTexturePath);
-
-    let metadataLength = 0;
-    if (os.endianness() === 'BE') {
-      metadataLength = originBuffer.readUInt32BE(PVR3_HEADER_META_DATA_LENGTH_OFFSET);
-    }
-    else {
-      metadataLength = originBuffer.readUInt32LE(PVR3_HEADER_META_DATA_LENGTH_OFFSET);
-    }
-    let newBuffer = Buffer.alloc(originBuffer.byteLength - metadataLength + PVR3_LAYA_MEATA_BYTE_LENGTH);
-   
-    originBuffer.copy(newBuffer, 0, 0, PVR3_HEADER_BYTE_LENGTH);
-    if (os.endianness() === 'BE') {
-      newBuffer.writeUInt32BE(PVR3_LAYA_MEATA_BYTE_LENGTH, PVR3_HEADER_META_DATA_LENGTH_OFFSET);
-    }
-    else {
-      newBuffer.writeUInt32LE(PVR3_LAYA_MEATA_BYTE_LENGTH, PVR3_HEADER_META_DATA_LENGTH_OFFSET);
-    }
- 
-    let offset = PVR3_HEADER_BYTE_LENGTH;
-
-    //_fourCC = LAYA
-    newBuffer.writeInt8(0x4c, offset); offset += 1;//L
-    newBuffer.writeInt8(0x41, offset); offset += 1;//A
-    newBuffer.writeInt8(0x59, offset); offset += 1;//Y
-    newBuffer.writeInt8(0x41, offset); offset += 1;//A
-    // _key = 0
-    newBuffer.writeUInt32LE(0, offset); offset += 4;
-    // _dataSize sizeof(rect)
-    newBuffer.writeUInt32LE(4 * 4, offset); offset += 4;
-    //_data rect
-    newBuffer.writeUInt32LE(x, offset); offset += 4;
-    newBuffer.writeUInt32LE(y, offset); offset += 4;
-    newBuffer.writeUInt32LE(width, offset); offset += 4;
-    newBuffer.writeUInt32LE(height, offset);
-
-    originBuffer.copy(newBuffer, PVR3_HEADER_BYTE_LENGTH + metadataLength, PVR3_HEADER_BYTE_LENGTH + PVR3_LAYA_MEATA_BYTE_LENGTH);
-    fs.writeFileSync(outputTexturePath, newBuffer);
+    cmd += "\"" + x + "\" ";
+    cmd += "\"" + y + "\" ";
+    cmd += "\"" + width + "\" ";
+    cmd += "\"" + height + "\" ";
+    child_process.execSync(cmd); 
   }
 }
 function isPVRTC(format: string): boolean {
@@ -138,39 +155,3 @@ function getAbsPath(dir: string): string {
     return dir;
   return path.join(process.cwd(), dir);
 }
-/*typedef struct
-{
-    uint32_t version;
-    uint32_t flags;
-    uint64_t pixelFormat;
-    uint32_t colorSpace;
-    uint32_t channelType;
-    uint32_t height;
-    uint32_t width;
-    uint32_t depth;
-    uint32_t numberOfSurfaces;
-    uint32_t numberOfFaces;
-    uint32_t numberOfMipmaps;
-    uint32_t metadataLength;
-} PVRv3TexHeader;
-*/
-
-const PVR3_HEADER_BYTE_LENGTH: number = 52;
-const PVR3_HEADER_META_DATA_LENGTH_OFFSET: number = 12 * 4;
-/*
-struct rect
-{
-  uint32_t x;  
-  uint32_t y;
-  uint32_t width;
-  uint32_t height;
-};
-struct LayaMeta
-{
-  uint32_t _fourCC;     // LAYA
-	uint32_t  _key;       // 0
-	uint32_t  _dataSize;  // sizeof(rect)
-  uint8_t*  _data;      // rect
-};
-*/
-const PVR3_LAYA_MEATA_BYTE_LENGTH: number = 4 * 3 + 4 * 4;
