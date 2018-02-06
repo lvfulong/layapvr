9cbcae8e9b4f39c484e0a300823c975ced79a9fe
lvfulong <775331175@qq.com>
2018-02-06 21:32:22 +0800

    --extrude 要求位数字类型

--- out/texturepackerCommand.js
+++ out/texturepackerCommand.js
@@ -85,6 +85,10 @@ exports.handler = function (argv) {
             console.log('当前只支持Windows平台');
             return;
         }
+        if (isNaN(argv.extrude)) {
+            console.log('错误: 参数 --extrude 要求位数字类型');
+            return;
+        }
         let files = fs.readdirSync(inputDir);
         for (let file of files) {
             let inputTexturePath = path.join(inputDir, file);
